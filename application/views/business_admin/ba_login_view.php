f0fa96db2d53cc04fcc731800115b7af031e3882
root <root@ip-172-31-74-110.ec2.internal>
2020-08-20 22:01:32 +0000

    sebd sns hide

--- application/views/business_admin/ba_login_view.php
+++ application/views/business_admin/ba_login_view.php
@@ -18,7 +18,7 @@
 						<div class="card-body">
 							<div class="m-sm-4">
 								<div class="text-center">
-									<img src="<?=base_url()?>public/images/Marks_Retech.png" alt="Cashier" class="img-fluid" />
+									<img src="<?=base_url()?>public/images/salonfirst.jpeg" alt="Cashier" class="img-fluid" />
 								</div>
 								<form id="BusinessAdminLogin" method="POST" action="#">
 									<div class="form-group">
